277475e741680610d8b4114a1778864dab87277e
Alan Lin <lin.alan.k@gmail.com>
2024-05-21 13:05:16 -0700

    silenced output from git for auto-git-update

--- auto-git-update/update.sh
+++ auto-git-update/update.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CURRENT_DATETIME=$(date +'%F %T');
 cd $SCRIPT_DIR;
-git add -A;
-git commit -m "update $CURRENT_DATETIME"
+git add -A &> /dev/null;
+git commit -m "update $CURRENT_DATETIME" &> /dev/null;
 
